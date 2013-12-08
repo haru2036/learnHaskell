cda94e07b058bf287cbd9986c0705b8419b65f6e
Haru2036 <haru2036@gmail.com>
2013-12-08 20:48:53 +0900

    一応1つずつのアイテムに対しての頻度数えられるようになった

--- test2.hs
+++ test2.hs
@@ -1,10 +1,11 @@
 import Data.List
+import Data.HashMap.Lazy
 
 searchAndCountWords :: String -> [String] -> Int
-getUnigramFrequency :: [String] -> [Int]
+getUnigramFrequency :: [String] -> HashMap String Int
 
-getUnigramFrequency sList= [searchAndCountWords a sList| a <- nub sList]
+getUnigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
 
-searchAndCountWords key items = length $ filter (==key) items
+searchAndCountWords key items = length $ Data.List.filter (==key) items
 
 main = return $ getUnigramFrequency ["a", "a", "ba", "bb", "ab", "a"]
