321563c07114b06784e26e252901a88c6698af1f
Haruka Nakajima <nakajima@zoysia.jp>
2013-12-12 20:48:41 +0900

    bigram的なのも数えられるようになった

--- test2.hs
+++ test2.hs
@@ -1,11 +1,20 @@
 import Data.List
+import Data.Tuple
 import Data.HashMap.Lazy
 
 searchAndCountWords :: String -> [String] -> Int
+searchAndCountWordsBigram :: (String, String) -> [(String, String)] -> Int
 getUnigramFrequency :: [String] -> HashMap String Int
+getBigramFrequency :: [(String, String)] -> HashMap (String, String) Int
+getBigramList :: [String] -> [(String, String)]
 
 getUnigramFrequency sList = fromList [(a, searchAndCountWords a sList) | a <- nub sList]
 
-searchAndCountWords key items = length $ Data.List.filter (==key) items
+getBigramList sList = if length sList > 2 then (sList !! 0, sList !! 1) : (getBigramList (tail sList))   else [(sList !! 0, sList !! 1)]
 
-main = return $ getUnigramFrequency ["a", "a", "ba", "bb", "ab", "a"]
+getBigramFrequency sLists = fromList [(a, searchAndCountWordsBigram a sLists) | a <- nub sLists]
+
+searchAndCountWords key items = length $ Data.List.filter (==key) items
+searchAndCountWordsBigram key items = length $ Data.List.filter (==key) items
+
+main = return $ getBigramFrequency (getBigramList ["a", "a", "ba", "bb", "ab", "a"])
